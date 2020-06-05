168757de05b0f0c71a6fa8c521b48d602b6ba811
marzeu <marzahell@hotmail.com>
2020-06-05 15:06:26 -0300

    rotas principais criadas

--- src/server.js
+++ src/server.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const server = express();
 
+server.use(express.static("public"));
+
+server.get("/", function (req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+server.get("/create-point", function (req, res) {
+    res.sendFile(__dirname + "/views/create-point.html");
+});
+
 server.listen(3000);
